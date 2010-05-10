f3fbf53fd9f4c78368897392c7e664151dca143e
mcrooney <mcrooney@dev.java.net>
2010-05-10 22:55:33 +0000

    [maven-scm] copy for tag htmlpublisher-0.3

---
+++ htmlpublisher/src/test/groovy/htmlpublisher/HtmlPublisherTest.groovy
@@ -0,0 +1,29 @@
+package htmlpublisher
+
+import org.jvnet.hudson.test.HudsonTestCase
+
+/**
+ *
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class HtmlPublisherTest extends HudsonTestCase {
+    /**
+     * Makes sure that the configuration survives the round trip.
+     */
+    public void testConfigRoundtrip() {
+        def p = createFreeStyleProject();
+        def l = [new HtmlPublisherTarget("a", "b", "c", true), new HtmlPublisherTarget("", "", "", false)]
+
+        p.publishersList.add(new HtmlPublisher(l));
+        submit(createWebClient().getPage(p,"configure").getFormByName("config"));
+
+        def r = p.publishersList.get(HtmlPublisher.class)
+        assertEquals(2,r.reportTargets.size())
+
+        (0..1).each {
+            assertEqualBeans(l[it],r.reportTargets[it],"reportName,reportDir,reportFiles,keepAll");
+        }
+    }
+
+}
