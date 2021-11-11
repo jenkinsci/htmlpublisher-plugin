422eea5c76933ba17aa143b0ab6cc215bac139f1
PatrickHess <94044227+PatrickHess@users.noreply.github.com>
2021-11-11 11:07:30 +0100

    Separate tests for file names with single and multiple slashes

--- src/test/java/htmlpublisher/HtmlFileNameTest.java
+++ src/test/java/htmlpublisher/HtmlFileNameTest.java
@@ -19,7 +19,33 @@ public class HtmlFileNameTest {
     public JenkinsRule j = new JenkinsRule();
 
     @Test
-    public void fileNameWithSpecialCharacters() throws Exception {
+    public void fileNameWithSpecialCharactersAndSingleSlash() throws Exception {
+        final String content = "<html><head><title>test</title></head><body>Hello world!</body></html>";
+
+        FreeStyleProject job = j.createFreeStyleProject();
+
+        job.getBuildersList().add(new CreateFileBuilder("subdir/#$&+,;= @.html", content));
+        job.getPublishersList().add(new HtmlPublisher(Arrays.asList(
+            new HtmlPublisherTarget("report-name", "", "subdir/*.html", true, true, false))));
+        job.save();
+
+        j.buildAndAssertSuccess(job);
+
+        JenkinsRule.WebClient client = j.createWebClient();
+        assertEquals(content,
+            client.getPage(job, "report-name/subdir/%23%24%26%2B%2C%3B%3D%20%40.html").getWebResponse().getContentAsString());
+
+        // published html page(s)
+        HtmlPage page = client.getPage(job, "report-name");
+        HtmlInlineFrame iframe = (HtmlInlineFrame) page.getElementById("myframe");
+        assertEquals("subdir/%23%24%26%2B%2C%3B%3D%20%40.html", iframe.getAttribute("src"));
+
+        HtmlPage pageInIframe = (HtmlPage) iframe.getEnclosedPage();
+        assertEquals("Hello world!", pageInIframe.getBody().asText());
+    }
+    
+    @Test
+    public void fileNameWithSpecialCharactersAndMultipleSlashes() throws Exception {
         final String content = "<html><head><title>test</title></head><body>Hello world!</body></html>";
 
         FreeStyleProject job = j.createFreeStyleProject();
