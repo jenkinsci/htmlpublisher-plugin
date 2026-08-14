cfcaf0235061c5e6e74610cba69534c268f7d648
Venkata Shreyas K <venkatashreyas.kabekkodu@fmr.com>
2026-08-14 11:12:46 +0530

    [JENKINS-76169] Inline JavaScript in wrapper HTML to fix empty iframe in downloaded zip reports (#590)
    * [JENKINS-76169] Inline JavaScript in wrapper HTML to fix empty iframe in downloaded zip reports
    
    Signed-off-by: Venkata Shreyas Kabekkodu <venkatashreyas.kabekkodu@fmr.com>

--- src/test/java/htmlpublisher/HtmlPublisherTest.java
+++ src/test/java/htmlpublisher/HtmlPublisherTest.java
@@ -1,8 +1,11 @@
 package htmlpublisher;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class
 HtmlPublisherTest {
@@ -35,6 +38,12 @@ HtmlPublisherTest {
         // test underscores not escaped when not requested
         assertEquals(HtmlPublisherTarget.sanitizeReportName("foo_bar", false), HtmlPublisherTarget.sanitizeReportName("foo_bar", false));
 
+    }
+
+    @Test
+    void testReadFileThrowsExceptionForMissingResource() {
+        assertThrows(IOException.class, () ->
+                HtmlPublisher.readFile("/nonexistent/resource.html", HtmlPublisher.class));
     }
 
     @Test
